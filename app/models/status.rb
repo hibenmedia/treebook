c612aae10b0f1ef02236daed654ecbb596feb219
Hiben Media <hibenmedia@gmail.com>
2013-11-24 16:27:06 -0500

    Added users and user friendships with associated tests.

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,7 +3,7 @@ class Status < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true,
-                      length: { minimum: 2 }
+                      length: { minimum: 1 }
 
   validates :user_id, presence: true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+	should have_many(:user_friendships)
+	should have_many(:friends)
+
 	test "a user should enter a first name" do
 		user = User.new
 		assert !user.save
@@ -44,5 +47,17 @@ class UserTest < ActiveSupport::TestCase
 
 		user.profile_name = 'jhipshir_1'
 		assert user.valid?
+	end
+
+	test 'that no error is raised when trying to get to access a friends list' do
+		assert_nothing_raised do
+			users(:jason).friends
+		end
+	end
+
+	test 'that creating friendships on a user works' do
+		users(:jason).friends << users(:mark)
+		users(:jason).friends.reload
+		assert users(:jason).friends.include?(users(:mark))
 	end
 end
